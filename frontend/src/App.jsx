7b70a08324d03353633a8596891bb1f4902833ca
hasaniisaher21 <sssiddiqui2102@gmail.com>
2024-08-27 19:26:41 +0530

    new

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import './App.css';
+import Navbar from "./components/shared/Navbar";
+
+
+function App() {
+ 
+
+  return (
+    <>
+      
+       <Navbar/> 
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Shared/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='bg-white'>
+            <h1 className='text-2xl font-bold'> Job<span>Portal</span></h1>
+        </div>
+    )
+}
+
+export default Navbar
